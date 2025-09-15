29f84742b0e3118daff45df676e3603e6f40acc2
Dan Schofield <dan@scho.ai>
2025-09-15 15:35:36 +0100

    add ultralytics yolo + bytetrack code for prototyping

--- object_detection/pipeline.py
+++ object_detection/pipeline.py
@@ -1,12 +1,16 @@
 from ultralytics import YOLO
 
 # Load your trained model
-model = YOLO("models/yolo/yolov8n-bees.pt")
+model = YOLO("../models/rachel_YOLOv8nano_feb25.pt")
 
 # Run inference on a video
 results = model.track(
-    source="C:/Users/you/Downloads/bee_video.mp4",  # path to your video
+    source="../data/Test2_20sec.MP4",  # path to your video
     tracker="bytetrack.yaml",  # enables tracking
-    show=True,   # display live
-    save=True    # save output video under runs/track/
+    show=False,   # display live
+    save=True,
+    project="results",
+    name="bee_tracking"    # save output video under runs/track/
 )
+
+print("Tracking complete. Results saved in the 'results/bee_tracking' directory.")
